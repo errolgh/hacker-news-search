aa2591f6af53c05b22f5c59f645a72fe7768c76b
Errol Watson <ewatson@ggoutfitters.com>
2020-11-09 09:02:02 -0500

    morning commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,4 @@
-const api = 'http://hn.algolia.com/api/v1/search'
-
-const API_SEARCH = `${api}?&hitsPerPage=15&tags=story&query=`
-const API_TOP = `${api}?tags=front_page&hitsPerPage=15`
+const API_SEARCH = `http://hn.algolia.com/api/v1/search?&hitsPerPage=15&tags=story&query=`
+const API_TOP = `http://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=15`
 
 export { API_TOP, API_SEARCH }
