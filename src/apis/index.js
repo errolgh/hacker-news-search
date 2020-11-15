c13882f3f508f1fb8c561d26503be81852d231f7
errolgh <ewatsonrei@gmail.com>
2020-11-15 11:08:10 -0500

    build prod

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const SEARCH_API = axios.create({
-    baseURL: 'http://hn.algolia.com/api/v1/search?&tags=story&page=1&hitsPerPage=15&query='
+    baseURL: 'https://hn.algolia.com/api/v1/search?&tags=story&page=1&hitsPerPage=15&query='
 })
 
 export const TOP_STORIES_API = axios.create({
-    baseURL: 'http://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=15'
+    baseURL: 'https://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=15'
 })
